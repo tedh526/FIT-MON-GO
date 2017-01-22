0b5d13b07e96e5bdc0d1b48e7981522faab9946a
Tedman Hsu <tedh526@gmail.com>
2017-01-22 14:22:38 -0500

    got maps working

--- components/avatar.js
+++ components/avatar.js
@@ -8,12 +8,14 @@ import {styles} from '../index.ios.js';
 
 export default Avatar = (props) => {
   let avatar = {
-    100: require('../images/ming/avatar_100.jpg'),
-    80: require('../images/ming/avatar_80.jpg'),
-    60: require('../images/ming/avatar_60.jpg'),
-    40: require('../images/ming/avatar_40.jpg'),
-    20: require('../images/ming/avatar_20.jpg'),
-    0: require('../images/ming/avatar_0.jpg')
+    100: require('../images/minions/avatar_100.jpg'),
+    80: require('../images/minions/avatar_80.jpg'),
+    60: require('../images/minions/avatar_60.jpg'),
+    40: require('../images/minions/avatar_40.jpg'),
+    20: require('../images/minions/avatar_20.jpg'),
+    0: require('../images/minions/avatar_0.jpg'),
+    eating: require('../images/minions/avatar_eating.jpg'),
+    workout: require('../images/minions/avatar_workout.jpg'),
   };
 
     // navigator.geolocation.getCurrentPosition((geolocation) => console.log('this is geolocation', geolocation));
@@ -22,7 +24,7 @@ export default Avatar = (props) => {
         <Text style={styles.welcome}>
           Avatar
         </Text>
-        <Image source={avatar[props.avatar]}/>
+        <Image source={avatar['eating']}/>
       </View>
     );
 };
